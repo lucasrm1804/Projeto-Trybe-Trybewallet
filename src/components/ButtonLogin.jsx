f1017152ca3007e6c99827916e56608d85889eba
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-01-11 11:35:47 -0300

    Requisito 1 - Crie uma página inicial de login - corrigi lint

--- src/components/ButtonLogin.jsx
+++ src/components/ButtonLogin.jsx
@@ -8,7 +8,7 @@ class ButtonLogin extends React.Component {
     const { addUser, email, isDisabled, history } = this.props;
     return (
       <button
-      disabled={ isDisabled }
+        disabled={ isDisabled }
         className={
           isDisabled
             ? `bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4
